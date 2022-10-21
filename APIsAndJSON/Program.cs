395ab2792edf5a0d79eaa87b8f36bb4e8cedf777
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-10-21 11:49:01 -0400

    finished assignment 1

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace APIsAndJSON
+﻿using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var https = new HttpClient();
+            string kanyeSaid;
+            string ronSaid;
+            string kanyeQuoted;
+            string ronQuoted;
+
+            for (int i = 0; i < 5; i++)
+            {
+                kanyeSaid = https.GetStringAsync("https://api.kanye.rest/").Result;
+                ronSaid = https.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+                kanyeQuoted = JObject.Parse(kanyeSaid).GetValue("quote").ToString();
+                ronQuoted = JArray.Parse(ronSaid).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                Console.WriteLine($"Kanye: {kanyeQuoted}");
+                Console.WriteLine($"Ron: {ronQuoted}");
+                Console.WriteLine("");
+            }
         }
     }
 }
